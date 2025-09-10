c05b3a25b93035d046f759b74fe7e809db16b001
mohammad <ekhlasimohammad345@gmail.com>
2025-09-10 19:21:51 +0200

    integrated google maps library

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,6 +1,16 @@
-import Button from '@mui/material/Button';
-
+import GoogleMapReact from 'google-map-react';
 import { useState } from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import './MapPage.css'
+
+const defaultMapProps = {
+    center: {
+      lat: 	51.260197,
+      lng: 4.402771
+    },
+    zoom: 11
+  };
 
 
 function MapPage() {
@@ -8,7 +18,16 @@ function MapPage() {
 
   return (
     <>
-      <Button variant="contained">This s map</Button>
+     <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCTTwaHdDb5GtrNJraHlZI6QEd6R1Mtqyg" }}
+        defaultCenter={defaultMapProps.center}
+        defaultZoom={defaultMapProps.zoom}
+      >
+        <LocationOnIcon lat="	51.260197" lng="4.402771" className="attraction-icon-box"/>
+      </GoogleMapReact>
+
+    </div>
     </>
   )
 }
