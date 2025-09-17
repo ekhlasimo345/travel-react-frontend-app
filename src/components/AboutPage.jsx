d179fac741d101b6e2c495fa9ed932577c421d63
mohammad <ekhlasimohammad345@gmail.com>
2025-09-17 18:00:44 +0200

    essentials for about page is added

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,13 +2,28 @@ import Button from '@mui/material/Button';
 
 import { useState } from 'react'
 
+import "./About.css"
+
 
 function AboutPage() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button variant="contained">This is about</Button>
+      <h1>About Us</h1>
+      <div className='aboutContainer'>
+        <h2>Mohammad</h2>  
+          <p>
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
+          </p>
+          <img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200" alt="userPhoto" 
+          />
+      </div>
     </>
   )
 }
