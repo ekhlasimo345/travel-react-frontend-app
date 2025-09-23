76ddbe8bece32a8d0cffb0897314f2d1441c322d
mohammad <ekhlasimohammad345@gmail.com>
2025-09-23 23:52:37 +0200

    contents added in about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,31 +1,72 @@
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
 
-import { useState } from 'react'
 
-import "./About.css"
+const cards = [
+  {
+    id: 1,
+    title: 'Mohammad',
+    description: 'front end developer '
+  },
+  {
+    id: 2,
+    title: 'Victoria',
+    description: 'back end developer',
+  },
+  {
+    id: 3,
+    title: 'Tri',
+    description: 'Text here',
+  },
+];
+
+ 
 
 
-function AboutPage() {
-  const [count, setCount] = useState(0)
+function AboutPage(){
+ 
 
-  return (
+
+  return(
     <>
-      <h1>About Us</h1>
-      <div className='aboutContainer'>
-        <h2>Mohammad</h2>  
-          <p>
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-            text heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext heretext here
-          </p>
-          <img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200" alt="userPhoto" 
-          />
-      </div>
+    <h1>About Us:</h1>
+        <Box
+      sx={{
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(min(300px, 100%), 1fr))',
+        gap: 2,
+      }}
+    >
+      {cards.map((card, index) => (
+        <Card>
+          <CardActionArea
+            sx={{
+              height: '100%',
+              '&[data-active]': {
+                backgroundColor: 'action.selected',
+                '&:hover': {
+                  backgroundColor: 'action.selectedHover',
+                  transform: 'scale(1.05)',
+                },
+              },
+            }}
+          >
+            <CardContent sx={{ height: '100%' }}>
+              <Typography variant="h5" component="div">
+                {card.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {card.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
     </>
-  )
-}
-
+)}
 export default AboutPage
