e7e264f1cc8c062d019f789f77c00018c7eb67c1
mohammad <ekhlasimohammad345@gmail.com>
2025-09-24 18:12:19 +0200

    some css changes and adding the slider in mappage

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -3,23 +3,32 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+import './AboutPage.css'
 
 
 const cards = [
   {
     id: 1,
     title: 'Mohammad',
-    description: 'front end developer '
+    image: 'https://maessencsc.com/wp-content/uploads/2024/08/mercedes-benz-g-klasse-w463-g63-2023-magno.jpg',
+    description: 'front end developer dvsvsnv sdvisfjsd ewsfjisdoiewf sdusfisjs dfsef sufsuf sefi efu9s uf9s dfdf',
+    socialMedia:'https://www.varzesh3.com/'
   },
   {
     id: 2,
     title: 'Victoria',
+    image: 'https://maessencsc.com/wp-content/uploads/2024/08/mercedes-benz-g-klasse-w463-g63-2023-magno.jpg',
     description: 'back end developer',
+    socialMedia:''
   },
   {
     id: 3,
     title: 'Tri',
+    image: 'https://maessencsc.com/wp-content/uploads/2024/08/mercedes-benz-g-klasse-w463-g63-2023-magno.jpg',
     description: 'Text here',
+    socialMedia:''
   },
 ];
 
@@ -36,9 +45,11 @@ function AboutPage(){
         <Box
       sx={{
         width: '100%',
+        borderRadius: 15,
         display: 'grid',
         gridTemplateColumns: 'repeat(auto-fill, minmax(min(300px, 100%), 1fr))',
         gap: 2,
+       
       }}
     >
       {cards.map((card, index) => (
@@ -46,22 +57,22 @@ function AboutPage(){
           <CardActionArea
             sx={{
               height: '100%',
-              '&[data-active]': {
-                backgroundColor: 'action.selected',
-                '&:hover': {
-                  backgroundColor: 'action.selectedHover',
-                  transform: 'scale(1.05)',
-                },
-              },
+               transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+               '&:hover': {
+               transform: 'scale(1.03)',
+               },
             }}
           >
             <CardContent sx={{ height: '100%' }}>
               <Typography variant="h5" component="div">
                 {card.title}
               </Typography>
+              
+              <img src={card.image} className='imageStyle'/>
               <Typography variant="body2" color="text.secondary">
                 {card.description}
               </Typography>
+              <a target="_blank"  href={card.socialMedia} ><LinkedInIcon/></a> 
             </CardContent>
           </CardActionArea>
         </Card>
--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -2,6 +2,8 @@ import GoogleMapReact from 'google-map-react';
 import { useState , useEffect } from 'react'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Tooltip from '@mui/material/Tooltip';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 
 
 import MapSearch from './MapSearch';
@@ -45,6 +47,19 @@ function MapPage() {
   
      <div style={{ height: '100vh', width: '100%' }}>
         <MapSearch searchCallback={ newSearch } />
+          <Box sx={{ width: 300 }}>
+      <Slider
+        aria-label="Temperature"
+        defaultValue={30}
+        //getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        shiftStep={30}
+        step={10}
+        marks
+        min={10}
+        max={100}
+      />
+    </Box>
       <GoogleMapReact
         bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY }}
         defaultCenter={defaultMapProps.center}
